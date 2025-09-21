6a4e7d6a3d4599eda8e7d7cf16efd3fef5c060df
sharmatitu12 <sharmatitu119@gmail.com>
2025-09-21 21:53:48 +0530

    sidebar fixed

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-  const menuBtn = document.getElementById("menu-btn");
-  const mobileMenu = document.getElementById("mobile-menu");
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+const overlay = document.getElementById("overlay");
+const closeBtn = document.getElementById("close-btn");
 
-  menuBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-  });
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("-translate-x-full");
+  overlay.classList.remove("hidden");
+});
 
+closeBtn.addEventListener("click", () => {
+  mobileMenu.classList.add("-translate-x-full");
+  overlay.classList.add("hidden");
+});
 
+overlay.addEventListener("click", () => {
+  mobileMenu.classList.add("-translate-x-full");
+  overlay.classList.add("hidden");
+});
